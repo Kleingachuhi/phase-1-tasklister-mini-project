50ba7ba86c44fd38011a72e78257679733c47fa1
Klein <klein.gachuhi@student.moringaschool.com>
2025-03-23 17:01:23 +0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+ let form = document.querySelector('#create-task-form')
+  let taskList = document.querySelector('#tasks')
+
+let formInput = document.querySelector('#new-task-description');
+formInput.value = ''
+let submit = document.querySelector('#submitBtn');
+form.addEventListener('submit',function(event){
+  event.preventDefault()
+  let newTask = document.createElement('li')
+  newTask.textContent = formInput.value;
+  taskList.appendChild(newTask)
+} )
+})
